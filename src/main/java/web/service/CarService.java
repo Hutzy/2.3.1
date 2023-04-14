f85fb21a3ff3caac25f49c1059611f0b3c197a95
Hutzy <whodoctor553@gmail.com>
2023-04-14 19:03:02 +0300

    интерфейс слабой связи зависимости через конструткор
    до меня немного дошло :)

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,27 +1,14 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.Arrays;
 import java.util.List;
 
-@Service
-public class CarService implements CarServiceImpl {
+public interface CarService {
 
-    private final List<Car> cars = Arrays.asList(
-            new Car("car1", "color1", 1900),
-            new Car("car2", "color2", 1901),
-            new Car("car3", "color3", 1902),
-            new Car("car4", "color4", 1903),
-            new Car("car5", "color5", 1904)
-    );
 
-    public List<Car> getAllCars() {
-        return cars;
-    }
+    public List<Car> getCars(int count);
 
-    public List<Car> getCars(int count) {
-        return cars.subList(0, count);
-    }
+    public List<Car> getAllCars();
 }
+
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,11 +1,29 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.Arrays;
 import java.util.List;
 
-public interface CarServiceImpl {
-    public List<Car> getCars(int count);
-}
+@Service
+public class CarServiceImpl implements CarService {
 
-//не до конца понял как это работает
+    private final List<Car> cars = Arrays.asList(
+            new Car("car1", "color1", 1900),
+            new Car("car2", "color2", 1901),
+            new Car("car3", "color3", 1902),
+            new Car("car4", "color4", 1903),
+            new Car("car5", "color5", 1904)
+    );
+
+    @Override
+    public List<Car> getAllCars() {
+        return cars;
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+        return cars.subList(0, count);
+    }
+}
